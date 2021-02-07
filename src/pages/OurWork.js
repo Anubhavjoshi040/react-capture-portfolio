55cc215c187b8f23fe81813b79191414ffcbd276
Anubhav Joshi <anubhavjoshi040@gmail.com>
2021-02-08 01:28:38 +0530

    11. Movie Details

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -13,21 +13,21 @@ const OurWork = () => {
             <Movie>
                 <h2>The Athlete</h2>
                 <div className="line"></div>
-                <Link>
+                <Link to="/work/the-athlete">
                     <img src={athlete} alt="athlete"/>
                 </Link>    
             </Movie>          
             <Movie>
                 <h2>The Racer</h2>
                 <div className="line"></div>
-                <Link>
+                <Link to="/work/the-racer">
                     <img src={theracer} alt="theracer"/>
                 </Link>    
             </Movie>          
             <Movie>
                 <h2>Good Times</h2>
                 <div className="line"></div>
-                <Link>
+                <Link to="/work/good-times">
                     <img src={goodtimes} alt="goodtimes"/>
                 </Link>    
             </Movie>          
@@ -41,6 +41,12 @@ const Work = styled.div`
     padding: 5rem 10rem;
     h2{
         padding: 1rem 0rem;
+        -webkit-touch-callout: none;
+        -webkit-user-select: none;
+        -khtml-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
     }
 `;
 const Movie = styled.div`
