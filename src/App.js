ea8f63a49873bd9da3aa6faeffc4c0f01ea54cee
Anubhav Joshi <anubhavjoshi040@gmail.com>
2021-02-18 18:28:36 +0530

    13. Page Transitions

--- src/App.js
+++ src/App.js
@@ -8,15 +8,19 @@ import ContactUs from './pages/ContactUs';
 import Nav from './components/Nav';
 import MovieDetail from './pages/MovieDetail';
 //Router
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, useLocation } from 'react-router-dom';
 import { About } from './styles';
+//Animations
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <Switch>
+      <AnimatePresence exitBeforeEnter>
+      <Switch location={location} key={location.pathname}>
         <Route path="/" exact>
           <AboutUs />
         </Route>
@@ -30,6 +34,7 @@ function App() {
           <ContactUs />
         </Route>
       </Switch>
+      </AnimatePresence>
     </div>
   );
 }
